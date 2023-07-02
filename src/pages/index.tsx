ac3deb027a9efe728be777e69368b67bf46b4384
Shitomo <cadaeibf@outlook.com>
2023-07-02 22:27:50 +0900

    feat add welcome message

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
-import { Button } from '@chakra-ui/react';
-import { signIn } from "next-auth/react";
+import { Button, Text } from '@chakra-ui/react';
+import { signIn, useSession } from "next-auth/react";
 
 export default function LoginPage() {
+  const session = useSession();
+
+  if (session.status === 'authenticated') {
+    return <Text fontSize='5xl'>Welcome!</Text>
+  }
+
   return (
       <Button onClick={() => signIn()}>Login with dream account</Button>
   );
