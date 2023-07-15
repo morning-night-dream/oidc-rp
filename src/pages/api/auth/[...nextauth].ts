071d0501adbdbab3f15ba0dd426d5e2ba98d1c6c
Shitomo <cadaeibf@outlook.com>
2023-07-15 15:34:15 +0900

    fix change signing method and set jwks endpoint

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -14,6 +14,7 @@ export const authOptions = {
         token: process.env.OP_URL + '/op/token',
         authorization: process.env.OP_URL + '/op/authorize?response_type=code',
         userInfo: process.env.OP_URL + '/op/userinfo',
+        jwks_endpoint: process.env.OP_URL + '/op/certs',
         checks: ['nonce' as const, 'state' as const],
         profile: (profile : any) => {
           return {
@@ -26,8 +27,8 @@ export const authOptions = {
         clientId: process.env.OP_CLIENT_ID,
         clientSecret: process.env.OP_CLIENT_SECRET,
         client: {
-          authorization_signed_response_alg: 'HS256',
-          id_token_signed_response_alg: 'HS256',
+          authorization_signed_response_alg: 'RS256',
+          id_token_signed_response_alg: 'RS256',
         },
     },
   ],
